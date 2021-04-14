10fd7e8132885392c30a0ba5ddbb29ad6c994395
imamnurby <43092949+imamnurby@users.noreply.github.com>
2021-04-14 11:54:56 +0800

    Create gesture_classifier.py

---
+++ IS708_api-with_hand_models/gesture_classifier.py
@@ -0,0 +1,45 @@
+import pickle
+import json
+import tsfel
+import os
+
+PATH = os.getcwd() + '/classifier_model/'
+GESTURE_MODEL_FILENAME = 'gesture_clf'
+SCALER_FILENAME = 'scaler'
+CONFIG = 'config.json'
+
+GESTURE_MODEL_PATHNAME = PATH + GESTURE_MODEL_FILENAME
+SCALER_PATHNAME = PATH + SCALER_FILENAME
+CONFIG_PATHNAME = PATH + CONFIG
+
+with open(CONFIG_PATHNAME, 'r') as j:
+    cfg_file = json.loads(j.read())
+
+classifier = pickle.load(open(GESTURE_MODEL_PATHNAME, 'rb'))
+scaler = pickle.load(open(SCALER_PATHNAME, 'rb'))
+
+
+def predict(raw_segment_dataframe):
+
+    # preprocess the signal: drop useless column, then pad the signal
+    signal_segment = raw_segment_dataframe.copy()
+    signal_segment = signal_segment.drop(["Timestamp", "A_X", "A_Y", "A_Z", "Gesture"], axis=1)
+
+    if signal_segment.shape[0] > 50:
+        signal_segment = signal_segment.iloc[:50,:]
+    else: 
+        signal_segment = signal_segment.reindex(range(50), fill_value=0)
+    
+    # extract features
+    fs=25
+    window_size=50
+    features = tsfel.time_series_features_extractor(cfg_file, signal_segment, fs=fs, window_size=window_size)
+    features = scaler.transform(features)
+
+    # predict gesture
+    gesture = classifier.predict(features)
+
+    return gesture
+
+if __name__ == '__main__':
+	pass
