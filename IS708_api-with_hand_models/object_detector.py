d89058820e2d0251bdd6f5f9f4365ad297c4d2e9
imamnurby <43092949+imamnurby@users.noreply.github.com>
2021-04-14 12:12:51 +0800

    Create object_detector.py

---
+++ IS708_api-with_hand_models/object_detector.py
@@ -0,0 +1,37 @@
+'''
+@author: gprana
+'''
+import cv2
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+
+def detect_objects(scene_image):
+    """Given scene image, this function detects objects in the scene
+    
+    Argument:
+    scene_image -- binary data of scene image
+    Return value:
+    list of tuples containing object name and their bounding boxes in format of
+    (obj_name, [bbox_coordinate]). E.g. ('chair',[0,0,100,100])
+    """
+    bbox, label, conf = cv.detect_common_objects(scene_image)
+    
+    detected_objects = list(zip(label, bbox))
+    detected_objects = [x for x in detected_objects if x[0]!="person"]
+    
+    return detected_objects
+
+if __name__ == '__main__':
+    for fname in ['test_image/test_image_01.JPG',
+                  'test_image/test_image_02.JPG',
+                  'test_image/test_image_03.JPG',
+                  'test_image/test_image_04.JPG',
+                  'test_image/test_image_05.JPG',
+                  'test_image/test_image_06.JPG']:   
+        try:
+            scene_image = cv2.imread(fname)
+            objects = detect_objects(scene_image)  
+            print(f"{fname} : {str(objects)}")
+        except:
+            #print(e)
+            print(f"Unable to detect objects in {fname}")
